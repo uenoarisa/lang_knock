5e362957f9c1cea0646106109eb7489ae598b07d
uenoarisa <arisa-choco0326@docomo.ne.jp>
2024-05-04 20:58:36 +0900

    change:42を関数化

--- ch05/quiz42.py
+++ ch05/quiz42.py
@@ -2,16 +2,21 @@
 # 係り元の文節と係り先の文節のテキストをタブ区切り形式ですべて抽出せよ．ただし，句読点などの記号は出力しないようにせよ．
 from quiz41 import parse_cabocha
 
+def extract_dependency(file_path):
+    sentences = parse_cabocha(file_path)
+    dependencies = []
+    for sentence in sentences:
+        for chunk in sentence:
+            src_surface = ''.join([morph.surface for morph in chunk.morphs if morph.pos != '記号'])
+            dst_surface = ''.join([morph.surface for morph in sentence[chunk.dst].morphs if morph.pos != '記号']) if chunk.dst != -1 else 'None'
+            src_surface = src_surface.replace('\t', '')
+            dst_surface = dst_surface.replace('\t', '')
+            dependencies.append((src_surface, dst_surface))
+    return dependencies
 
 if __name__ == '__main__':
     sentences = parse_cabocha('../ai.ja.txt.parsed')
-    for sentence in sentences:
-        for chunk in sentence:
-            # 文節の文字列を作成
-            chunk_surface = ''.join([morph.surface for morph in chunk.morphs])
-            
-            # 係り先の表示
-            dst_surface = ''.join([morph.surface for morph in sentence[chunk.dst].morphs]) if chunk.dst != -1 else 'None'
-            
-            print(f'文節: {chunk_surface}  係り先: {dst_surface}')
-        print('-----------------------------')
+    dependencies = extract_dependency('../ai.ja.txt.parsed')
+    for src, dst in dependencies:
+        print(f'{src}\t{dst}')
+    print('-----------------------------')
