21eb5938570cfd8de6fa759358af82f407ef1434
uenoarisa <arisa-choco0326@docomo.ne.jp>
2024-05-04 20:58:49 +0900

    43完了

---
+++ ch05/quiz43.py
@@ -0,0 +1,28 @@
+# 43. 名詞を含む文節が動詞を含む文節に係るものを抽出
+# 名詞を含む文節が，動詞を含む文節に係るとき，これらをタブ区切り形式で抽出せよ．ただし，句読点などの記号は出力しないようにせよ．
+from quiz41 import parse_cabocha
+
+def extract_SV(file_path):
+    sentences = parse_cabocha(file_path)
+    dependencies = []
+    for sentence in sentences:
+        for chunk in sentence:
+            # 係り先が動詞を含む文節かどうかをチェック
+            if chunk.dst != -1 and any(morph.pos == '動詞' for morph in sentence[chunk.dst].morphs):
+                # 係り元が名詞を含む文節かどうかをチェック
+                if any(morph.pos == '名詞' for morph in chunk.morphs):
+                    src_surface = ''.join([morph.surface for morph in chunk.morphs if morph.pos != '記号'])
+                    dst_surface = ''.join([morph.surface for morph in sentence[chunk.dst].morphs if morph.pos != '記号'])
+                    src_surface = src_surface.replace('\t', '')
+                    dst_surface = dst_surface.replace('\t', '')
+                    dependencies.append((src_surface, dst_surface))
+    return dependencies
+
+    
+
+
+if __name__ == '__main__':
+    dependencies = extract_SV('../ai.ja.txt.parsed')
+    for src, dst in dependencies:
+        print(f'{src}\t{dst}')
+    print('-----------------------------')
